d0046a621d1d8daa5f2aa4541034a4d77259e6bb
John-Kimani <kimwaweru100@gmail.com>
2022-04-07 23:47:38 +0300

    Dev:Logout

--- users/views.py
+++ users/views.py
@@ -8,8 +8,8 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created form {username}')
-            return redirect('homepage')
+            messages.success(request, f'Your account has been created. You can now login.')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form':form})
