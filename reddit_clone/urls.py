780428f8812bd301ac65c0d560804394461df938
John-Kimani <kimwaweru100@gmail.com>
2022-04-10 17:01:28 +0300

    Dev: start api

--- reddit_clone/urls.py
+++ reddit_clone/urls.py
@@ -21,6 +21,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('app/', include('reddit.urls')), 
+    path('api/', include('api.urls')),
 
     path('register/', user_views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
